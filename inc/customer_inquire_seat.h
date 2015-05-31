0cfba387477e93ea375fb603a06ba6d61571ad4d
SeongyongChang <chang7290@gmail.com>
2015-05-31 17:54:22 +0900

    commit test

---
+++ inc/customer_inquire_seat.h
@@ -0,0 +1,21 @@
+/*
+ * customer_inquire_seat.h
+ *
+ *  Created on: May 31, 2015
+ *      Author: seongyong
+ */
+
+#ifndef CUSTOMER_INQUIRE_SEAT_H_
+#define CUSTOMER_INQUIRE_SEAT_H_
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+#include "tass.h"
+#include "customer.h"
+
+Evas_Object* add_customer_inquire_seat_view(Evas_Object *ad);
+
+#endif /* CUSTOMER_INQUIRE_SEAT_H_ */
---
+++ src/customer_inquire_seat.c
@@ -0,0 +1,12 @@
+/*
+ * customer_inquire_seat.c
+ *
+ *  Created on: May 31, 2015
+ *      Author: seongyong
+ */
+
+#include "customer_inquire_seat.h"
+
+Evas_Object* add_customer_inquire_seat_view(Evas_Object *ad){
+
+}
